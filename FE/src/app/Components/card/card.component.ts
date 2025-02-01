2c81c0c52e49ac9bedc72d57f74a2a3413a5cf1f
ChandaniM <chandanimourya5@gmail.com>
2025-02-01 12:52:30 +0530

    - Remove the comments

--- FE/src/app/Components/card/card.component.ts
+++ FE/src/app/Components/card/card.component.ts
@@ -9,18 +9,7 @@ import { Product } from 'src/app/models/product';
 export class CardComponent{
   @Input() product: Product | null = null;
   isWishlisted = false;
-  // ngOnInit(): void {
-    
-  // }
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   if (changes['product'] && changes['product'].currentValue) {
-  //     console.log('Current Value:', changes['product'].currentValue);
-  //     console.log('Previous Value:', changes['product'].previousValue);
-  //   }
-  // }
-  // ngOnDestroy(): void {
-    
-  // }
+  
   toggleWishlist() {
     this.isWishlisted = !this.isWishlisted;
   }
