1bff224454c02cb026b489606feafb00ae79ff47
ChandaniM <chandanimourya5@gmail.com>
2025-02-18 19:40:04 +0530

    - Added the category card of product

--- FE/src/app/Pages/dashboard/dashboard.component.ts
+++ FE/src/app/Pages/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CarouselComponent } from '../../Components/carousel/carousel.component';
 import { CardComponent } from '../../Components/card/card.component';
 import { CommonModule } from '@angular/common';
+import { FeatureCardComponent } from '../../Components/feature-card/feature-card.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CardComponent ,CarouselComponent , CommonModule],
+  imports: [CardComponent ,CarouselComponent , CommonModule , FeatureCardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
@@ -30,41 +31,181 @@ export class DashboardComponent {
       }
     },
     {
-      "name": "Multi Grain Combo Cookies",
-      "description": "Healthy and delicious multi-grain cookies.",
-      "price": 25,
-      "image": "/images/brandlogo.png",
-      "rating": 4.5,
-      "sale": true,
-      "grams": ["250g", "500g"],
-      "layout": "landscape",
-      "features": {
-        "cardWidth": "800px",
-        "button": { "show": true, "text": "Add To Cart" },
-        "gramsTags": true,
-        "input": { "show": true, "type": "number" },
-        "titleAlignment": "left",
-        "buttonWrapper": "flex-row"
-      }
+        "name": "Multi Grain Combo Cookies",
+        "description": "Healthy and delicious multi-grain cookies.",
+        "price": 25,
+        "image": "/images/brandlogo.png",
+        "rating": 4.5,
+        "sale": true,
+        "grams": [],
+        "layout": "portrait",
+        "features": {
+          "cardWidth": null,
+          "button": { "show": false, "text": "" },
+          "gramsTags": false,
+          "input": { "show": false, "type": "" },
+          "titleAlignment": "left",
+          "buttonWrapper": "flex-row"
+        }
+      },
+      {
+        "name": "Multi Grain Combo Cookies",
+        "description": "Healthy and delicious multi-grain cookies.",
+        "price": 25,
+        "image": "/images/brandlogo.png",
+        "rating": 4.5,
+        "sale": true,
+        "grams": [],
+        "layout": "portrait",
+        "features": {
+          "cardWidth": null,
+          "button": { "show": false, "text": "" },
+          "gramsTags": false,
+          "input": { "show": false, "type": "" },
+          "titleAlignment": "left",
+          "buttonWrapper": "flex-row"
+        }
+      },
+      {
+        "name": "Multi Grain Combo Cookies",
+        "description": "Healthy and delicious multi-grain cookies.",
+        "price": 25,
+        "image": "/images/brandlogo.png",
+        "rating": 4.5,
+        "sale": true,
+        "grams": [],
+        "layout": "portrait",
+        "features": {
+          "cardWidth": null,
+          "button": { "show": false, "text": "" },
+          "gramsTags": false,
+          "input": { "show": false, "type": "" },
+          "titleAlignment": "left",
+          "buttonWrapper": "flex-row"
+        }
+      },
+      {
+        "name": "Multi Grain Combo Cookies",
+        "description": "Healthy and delicious multi-grain cookies.",
+        "price": 25,
+        "image": "/images/brandlogo.png",
+        "rating": 4.5,
+        "sale": true,
+        "grams": [],
+        "layout": "portrait",
+        "features": {
+          "cardWidth": null,
+          "button": { "show": false, "text": "" },
+          "gramsTags": false,
+          "input": { "show": false, "type": "" },
+          "titleAlignment": "left",
+          "buttonWrapper": "flex-row"
+        }
+      },
+      {
+        "name": "Multi Grain Combo Cookies",
+        "description": "Healthy and delicious multi-grain cookies.",
+        "price": 25,
+        "image": "/images/brandlogo.png",
+        "rating": 4.5,
+        "sale": true,
+        "grams": [],
+        "layout": "portrait",
+        "features": {
+          "cardWidth": null,
+          "button": { "show": false, "text": "" },
+          "gramsTags": false,
+          "input": { "show": false, "type": "" },
+          "titleAlignment": "left",
+          "buttonWrapper": "flex-row"
+        }
+      },
+      {
+        "name": "Multi Grain Combo Cookies",
+        "description": "Healthy and delicious multi-grain cookies.",
+        "price": 25,
+        "image": "/images/brandlogo.png",
+        "rating": 4.5,
+        "sale": true,
+        "grams": [],
+        "layout": "portrait",
+        "features": {
+          "cardWidth": null,
+          "button": { "show": false, "text": "" },
+          "gramsTags": false,
+          "input": { "show": false, "type": "" },
+          "titleAlignment": "left",
+          "buttonWrapper": "flex-row"
+        }
+      },
+      {
+        "name": "Multi Grain Combo Cookies",
+        "description": "Healthy and delicious multi-grain cookies.",
+        "price": 25,
+        "image": "/images/brandlogo.png",
+        "rating": 4.5,
+        "sale": true,
+        "grams": [],
+        "layout": "portrait",
+        "features": {
+          "cardWidth": null,
+          "button": { "show": false, "text": "" },
+          "gramsTags": false,
+          "input": { "show": false, "type": "" },
+          "titleAlignment": "left",
+          "buttonWrapper": "flex-row"
+        }
+      },
+  ];
+  categoryProduct = [
+    {
+      image: "/images/peaches.png",
+      title: "Fruits",
+      totalCount: "320",
+      tag : "true",
+      tagValue : "30",
+      gradientColor: "linear-gradient(to bottom, #FFF6EC, #FFFFFF)" // Light Peach
     },
     {
-      "name": "Newsletter",
-      "description": "Subscribe to Masterkart for future updates.",
-      "price": 0,
-      "image": "/images/brandlogo.png",
-      "rating": 0,
-      "sale": false,
-      "grams": [],
-      "layout": "landscape",
-      "features": {
-        "cardWidth": null,
-        "button": { "show": true, "text": "Subscribe" },
-        "gramsTags": false,
-        "input": { "show": true, "type": "text" },
-        "titleAlignment": "center",
-        "buttonWrapper": "flex-column"
-      }
+      image: "/images/bread.png",
+      title: "Bakery",
+      totalCount: "320",
+      tag : "true",
+      tagValue : "30",
+      gradientColor: "linear-gradient(to bottom, #E2FDE2, #FFFFFF)" // Light Green
+    },
+    {
+      image: "/images/vegetable.png",
+      title: "Vegetables",
+      totalCount: "320",
+      tag : "true",
+      tagValue : "30",
+      gradientColor: "linear-gradient(to bottom, #FEEAE8, #FFFFFF)" // Light Yellowish-Pink
+    },
+    {
+      image: "/images/milk.png",
+      title: "Dairy & Milk",
+      totalCount: "320",
+      tag : "true",
+      tagValue : "30",
+      gradientColor: "linear-gradient(to bottom, #FDE1F5, #FFFFFF)" // Light Pink
+    },
+    {
+      image: "/images/french-fries.png",
+      title: "Snack & Spice",
+      totalCount: "320",
+      tag : "true",
+      tagValue : "30",
+      gradientColor: "linear-gradient(to bottom, #ECF0FF, #FFFFFF)" // Light Blue
+    },
+    {
+      image: "/images/orange-juice.png",
+      title: "Juice & Drinks ",
+      totalCount: "320",
+      tag : "true",
+      tagValue : "30",
+      gradientColor: "linear-gradient(to bottom, #F9F9D9, #FFFFFF)" // Light Yellow
     }
-  ]
+  ];
   
 }
--- FE/src/app/app.config.ts
+++ FE/src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { routes } from './app.routes';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ]
 };
