ad16b410a2af0c01eebd55e3faa9f5d1bab07ca7
ChandaniM <chandanimourya5@gmail.com>
2025-03-11 00:37:05 +0530

    - Frontend user table data get from api

---
+++ FE/src/app/Services/response-mapper-service.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { UsersService } from './users.service';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResponseMapperServiceService {
+
+  constructor(private userService  :UsersService) { }
+
+
+  getModifyUserList() {
+    return this.userService.getAllUserList().pipe(map(userData => this.transformUserData(userData)));
+  }
+  
+  transformUserData(apiData: any[]): any {
+    return {
+      header: [
+        { name: "Username" },
+        { name: "Email" },
+        { name: "Password" },
+        { name: "First Name" },
+        { name: "Last Name" },
+        { name: "Phone Number" },
+        { name: "Country" },
+        { name: "Place" },
+        { name: "Address" },
+        { name: "Postal Code" },
+        { name: "Date of Birth" },
+        { name: "Wallet Balance" },
+        { name: "Is Active" },
+        { name: "Created At" },
+        { name: "Updated At" },
+        { name: "Role" },
+        { name: "Settings" }
+      ],
+      tableData: apiData.map(user => ({
+        "Username": { value: user.username },
+        "Email": { value: user.email },
+        "Password": "********",  // Masked Password
+        "First Name": user.first_name,
+        "Last Name": user.last_name,
+        "Phone Number": { value: user.phone_number },
+        "Country": user.country,
+        "Place": user.place,
+        "Address": user.address,
+        "Postal Code": user.postal_code,
+        "Date of Birth": { value: new Date(user.date_of_birth).toISOString().split('T')[0] }, // Format Date
+        "Wallet Balance": { value: parseFloat(user.wallet_balance) },
+        "Is Active": user.is_active ? "yes" : "no",
+        "Created At": new Date(user.created_at).toLocaleString(),
+        "Updated At": new Date(user.updated_at).toLocaleString(),
+        "Role": user.role,
+        "Settings": { icon: "bi bi-gear", onAction: true }
+      }))
+    };
+  }
+}
--- FE/src/app/Services/users.service.ts
+++ FE/src/app/Services/users.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-
-
+  apiUrl = environment.apiUrl;
+  private readonly http: HttpClient = inject(HttpClient);
   constructor() { }
 
-    
+    getAllUserList(){
+      return this.http.get<Array<any>>(this.apiUrl +`user-list`);
+    }
 
 }
