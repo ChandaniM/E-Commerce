b5d3e304b8bd2f6eb2429f44d4d8bc973ec1457f
ChandaniM <chandanimourya5@gmail.com>
2025-03-16 17:01:05 +0530

    - dont change anything

--- FE/src/app/Components/dynamic-form/dynamic-form.component.ts
+++ FE/src/app/Components/dynamic-form/dynamic-form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ResponseMapperServiceService } from '../../Services/response-mapper-service.service';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -12,10 +13,15 @@ export class DynamicFormComponent {
   
     @Input() formFields: any[] = []; // Parent se data aayega
     userForm: FormGroup = new FormGroup({});
-  
-    constructor(private fb: FormBuilder) {}
-  
+    @Output() formSubmitted = new EventEmitter<any>();
+    constructor(private fb: FormBuilder , private mapperService : ResponseMapperServiceService) {}
     ngOnInit() {
+      // this.mapperService.userDetailsSub.subscribe(userDetails => {
+      //   if (userDetails) {
+      //     this.patchFormValues(userDetails);
+      //   }
+      // });
+  
       this.createForm();
     }
   
@@ -29,14 +35,21 @@ export class DynamicFormComponent {
       this.userForm = this.fb.group({});
       this.formFields.forEach(field => {
         const validators = field.required ? [Validators.required] : [];
-        this.userForm.addControl(field.name, this.fb.control('', validators));
+        this.userForm.addControl(field.name, this.fb.control(field.value || '', validators)); // Default value set
       });
+    }
+  
+    patchFormValues(data: any) {
+      if (this.userForm) {
+        this.userForm.patchValue(data);
+      }
     }
   
     onSubmit() {
       if (this.userForm.valid) {
         console.log(this.userForm.value);
         alert('Form Submitted Successfully 🎯');
+        this.formSubmitted.emit(this.userForm.value)
       } else {
         alert('Please fill all required fields');
       }
