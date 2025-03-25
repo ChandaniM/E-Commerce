2708ebd0ffdf309ae6107713e28fac7bad8ed7a9
ChandaniM <chandanimourya5@gmail.com>
2025-03-25 21:04:28 +0530

    chore: remove backend code from frontend branch

--- BE/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
-router.get("/products", productController.getAllProduct);
-router.post("/addToCart", productController.addToCartController);
-router.post("/addProduct", productController.addProductController);
-router.post("/wishlist", productController.addToWishlistController);
-router.get('/getCartData', productController.getCartListController);
-module.exports = router;
